fe42e9ff326b092ec6c5e9f8cab646b15f8c90b3
Nazeer Allahham <nazeer12allaham@gmail.com>
2020-07-09 22:56:39 +0300

    passport migratio

---
+++ database/migrations/2020_07_09_192856_passport.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Passport extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth_access_tokens', function (Blueprint $table) {
+            $table->string('id')->nullable(false);
+            $table->bigInteger('user_id')->nullable(false)->index();
+            $table->bigInteger('client_id')->nullable(false);
+
+            $table->string('name');
+            $table->text('scopes');
+            $table->boolean('revoked')->nullable(false);
+
+            $table->timestamp('created_at', 0);
+            $table->timestamp('updated_at', 0);
+            $table->timestamp('expires_at', 0);
+        });
+
+        Schema::create('oauth_auth_codes', function (Blueprint $table) {
+            $table->string('id')->nullable(false);
+            $table->bigInteger('user_id')->nullable(false)->index();
+            $table->bigInteger('client_id')->nullable(false);
+
+            $table->text('scopes');
+            $table->boolean('revoked')->nullable(false);
+
+            $table->timestamp('expires_at', 0);
+        });
+
+        Schema::create('oauth_clients', function (Blueprint $table) {
+            $table->bigInteger('id')->nullable(false);
+            $table->bigInteger('user_id');
+
+            $table->string('name')->nullable(false);
+            $table->string('secret');
+            $table->string('provider');
+            $table->text('redirect')->nullable(false);
+            $table->boolean('personal_access_client')->nullable(false);
+            $table->boolean('password_client')->nullable(false);
+            $table->boolean('revoked')->nullable(false);
+
+            $table->timestamp('created_at', 0);
+            $table->timestamp('updated_at', 0);
+        });
+
+        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('client_id')->nullable(false);
+
+            $table->timestamp('created_at', 0);
+            $table->timestamp('updated_at', 0);
+        });
+
+        Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
+            $table->string('id')->nullable(false);
+            $table->string('access_token_id')->nullable(false);
+
+            $table->boolean('revoked')->nullable(false);
+            $table->timestamp('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oauth_access_tokens');
+        Schema::dropIfExists('oauth_auth_codes');
+        Schema::dropIfExists('oauth_clients');
+        Schema::dropIfExists('oauth_personal_access_clients');
+        Schema::dropIfExists('oauth_refresh_tokens');
+    }
+}
