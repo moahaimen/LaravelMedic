75373ce8235dc6e8c95defc667b4bf9cbd90cfd6
Nazeer Allahham <nazeer12allaham@gmail.com>
2020-08-17 01:12:16 +0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="./assets/img/apple-icon.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="./assets/img/logo.png"
-        />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-            name="viewport"
-        />
-        <meta name="viewport" content="width=device-width" />
-        <meta name="author" content="Nazeer Allahham" />
-        <meta name="description" content="Medical products store" />
 
-        <title>Molar Dent 2020</title>
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="./assets/img/logo.png" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta name="viewport" content="width=device-width" />
+  <meta name="author" content="Nazeer Allahham" />
+  <meta name="description" content="Medical products store" />
 
-        <base href="/" />
+  <title>Molar Dent 2020</title>
 
-        <link rel="manifest" href="/manifest.json" />
+  <base href="/" />
 
-        <!--     Fonts and icons     -->
-        <link href="/assets/css/montserrat.font.css" rel="stylesheet" />
-        <link href="/assets/css/font-awesome.min.css" rel="stylesheet" />
-        <link rel="manifest" href="manifest.webmanifest" />
-        <meta name="theme-color" content="#1976d2" />
-        <link rel="stylesheet" href="styles.43af0b652ffcd0a41e6b.css" />
-    </head>
+  <link rel="manifest" href="/manifest.json" />
 
-    <body>
-        <app-root>
-            <div class="loader-container">
-                <div class="loader">Loading</div>
-            </div>
-        </app-root>
-        <noscript
-            >Please enable JavaScript to continue using this
+  <!--     Fonts and icons     -->
+  <link href="/assets/css/montserrat.font.css" rel="stylesheet" />
+  <link href="/assets/css/font-awesome.min.css" rel="stylesheet" />
+  <link rel="manifest" href="manifest.webmanifest" />
+  <meta name="theme-color" content="#1976d2" />
+  <link rel="stylesheet" href="styles.43af0b652ffcd0a41e6b.css" />
+</head>
+
+<body>
+  <app-root>
+    <div class="loader-container">
+      <div class="loader">Loading</div>
+    </div>
+  </app-root>
+  <noscript>Please enable JavaScript to continue using this
             application.</noscript
         >
         <script src="runtime.208fc229010233c7ee70.js" defer></script>
