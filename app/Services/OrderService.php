c69f461a8e790e64027bd58c18e7330c552d1e22
Nazeer Allahham <nazeer12allaham@gmail.com>
2020-12-24 23:18:53 +0200

    Fix var keyword problem

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class OrderService extends Service
 {
-    var NotificationService $notifier;
+    protected $notifier;
 
     public function __construct(NotificationService $notifier)
     {
